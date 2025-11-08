f079466cef301253a001f5d16cfcd9fff7e571cf
arcamp0130 <arcmp0130@gmail.com>
2025-11-08 01:25:26 -0600

    feat: initializing express

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello from node!")
+import express from "express"
+
+// TODO: import from .env
+const PORT = 3030 
+const ENVIRONMENT =  "development"
+// end TODO
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`${ENVIRONMENT} server running at http://localhost:${PORT}`)
+})

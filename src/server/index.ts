6df40b34bdf637f610b19dedecec2185380f788f
arcamp0130 <arcmp0130@gmail.com>
2025-11-09 23:40:15 -0600

    feat: Staring with Express routing.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Router } from "express"
 
 // TODO: import from .env
 const PORT = 3030 
@@ -6,6 +6,12 @@ const ENVIRONMENT =  "development"
 // end TODO
 
 const app = express()
+const router = Router()
+
+router.get("/", (_: express.Request, res: express.Response) => {
+    res.send("Hello from Express and TS!")
+})
+app.use("/", router)
 
 app.listen(PORT, () => {
     console.log(`${ENVIRONMENT} server running at http://localhost:${PORT}`)

902f447ca34fdd73c326d5de2248fc5f04377c98
arcamp0130 <arcmp0130@gmail.com>
2025-11-11 14:13:12 -0600

    feat: using es6-fuzzy lib to import shape in main service.

---
+++ src/server/components/fuzzy.shapes.ts
@@ -0,0 +1,10 @@
+import { Triangle } from "es6-fuzz/lib/curve/triangle"
+import { Trapezoid } from "es6-fuzz/lib/curve/trapezoid"
+import { Constant } from "es6-fuzz/lib/curve/constant"
+import { Grade } from "es6-fuzz/lib/curve/grade"
+import { ReverseGrade } from "es6-fuzz/lib/curve/reverse-grade"
+import { Sigmoid } from "es6-fuzz/lib/curve/sigmoid"
+
+export default {
+    Triangle, Trapezoid, Constant, Grade, ReverseGrade, Sigmoid
+}
---
+++ src/server/services/fuzzy.service.ts
@@ -0,0 +1,12 @@
+import shapes from "../components/fuzzy.shapes.ts"
+
+export class FuzzyTip {
+    // Empty cosntructor
+    constructor() {}
+
+    public calculate (): number {
+        let percentage = 15.7 // Mock
+
+        return percentage
+    }
+}

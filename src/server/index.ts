9b13c69ede2b1bc1dbc7a0965ff3e500b8d09fc4
arcamp0130 <arcmp0130@gmail.com>
2025-11-10 23:19:09 -0600

    feat: using CORS and json-formatted data to handle frontend interactions.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,13 @@
-import express, { Router } from "express"
+import express from "express"
+import cors from "cors"
 import { PORT, ENVIRONMENT } from "./secrets.ts"
 import routes from "./routes/index.routes.ts"
 
 const app = express()
 
+app.use(cors()) // Allowing requests from frontend at any domain
+app.use(express.json()) // Allowing json-formatted bodies
+app.use(express.urlencoded()) // Parsing from URL options to JSON
 app.use("/", routes)
 
 app.listen(PORT, () => {
